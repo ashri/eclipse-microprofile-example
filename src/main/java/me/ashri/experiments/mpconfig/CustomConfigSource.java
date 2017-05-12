b8d9a7cd11c913c822161d6afd2c296edcd78849
Ashley Richardson <ash@threeheadedmonkey.com>
2017-05-12 11:06:32 +0800

    Add logging to CustomConfigSource to see when properties are returned

--- src/main/java/me/ashri/experiments/mpconfig/CustomConfigSource.java
+++ src/main/java/me/ashri/experiments/mpconfig/CustomConfigSource.java
@@ -4,8 +4,11 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class CustomConfigSource implements ConfigSource {
+
+    private final static Logger LOG = Logger.getLogger(CustomConfigSource.class.getName());
 
     private final Map<String, String> properties;
 
@@ -16,11 +19,13 @@ public class CustomConfigSource implements ConfigSource {
 
     @Override
     public Map<String, String> getProperties() {
+        LOG.info("Returning all properties");
         return properties;
     }
 
     @Override
     public String getValue(String s) {
+        LOG.info("Returning property for " + s);
         return properties.get(s);
     }
 
