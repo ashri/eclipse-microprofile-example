1fd62ee64e248617229576134366a318e325fc1d
Ashley Richardson <ash@threeheadedmonkey.com>
2017-05-12 10:58:17 +0800

    Introduce properties file-based configuration provided by Microprofile Config

--- src/main/java/me/ashri/experiments/mpconfig/GreetingProducer.java
+++ src/main/java/me/ashri/experiments/mpconfig/GreetingProducer.java
@@ -1,14 +1,21 @@
 package me.ashri.experiments.mpconfig;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 public class GreetingProducer {
 
+    @Inject
+    @ConfigProperty(name = "greeting.word")
+    String word;
+
     @Produces
     @Named("greeting")
     public String greeting() {
-        return "Hello";
+        return word;
     }
 
 }
